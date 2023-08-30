3b86613661e5ec46d5900d3d04e4bea057e31459
Luigi Rauso <darjkzer@hotmail.it>
2023-08-30 19:36:37 +0200

    json methods 1 - exercise completed

---
+++ 29. JSON Methods 1/exercise.js
@@ -0,0 +1,16 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+const developer = new Person(1, "Mario", "Rossi", 25);
+
+// Print developer as json object
+// Soluzione:
+let objJSON = JSON.stringify(developer);
+
+console.log(objJSON);
